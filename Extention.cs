bba51604f18031060879ba2a953dff1bb76fed96
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2022-04-23 18:04:21 +0400

    Add files via upload

---
+++ Extention.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    static class Extention
+    {
+        public static string Capitalize(this string word )
+        {
+            if (word == null) return null;
+            return char.ToUpper(word[0]) + word.Substring(1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "aslan";
+            Console.WriteLine(word.Capitalize());
+        }
+     
+
+    }
+   
+}
+
+
